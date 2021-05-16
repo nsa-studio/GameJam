180c57b0b325aed05b33eadea4c4f5e64c06cf2a
nsa-studio <Paladin2015@yandex.ru>
2021-05-16 13:15:39 +0300

    All Add
    All Add

---
+++ GameJam1/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject MenuUI;
+    public void MineMenu()
+    {
+        
+        Debug.Log("Главное меню");
+        SceneManager.LoadScene(0);
+
+    }
+    public void RestartGame()
+    {
+
+        Debug.Log("Перезапуск уровня");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Перезагрузка сцены
+        
+    }
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+        MenuUI.gameObject.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Выход");
+        Application.Quit();
+
+    }
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        MenuUI.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Меню");
+            Time.timeScale = 0;
+            MenuUI.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ GameJam1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void NewGame() 
+    {
+        
+        Debug.Log("Новая игра");
+        SceneManager.LoadScene(1);
+        
+    }
+
+    public void ExitGame() 
+    {
+        Debug.Log("Выход");
+        Application.Quit();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
